9425ce70f04afc85ddef5554cba342d462bd7090
ayushbhavsar21 <ayush21bhavsar@gmail.com>
2024-03-18 01:31:42 +0530

    Reinstalled myenv

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ayush/Dehaze/myenv"
+set -gx VIRTUAL_ENV "/home/ayush/Real-time-Dehaze/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
